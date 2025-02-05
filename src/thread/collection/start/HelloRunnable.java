15ff897c43abb82d197e9281c8eb0925febcccf8
drnomader <drnomader@gmail.com>
2025-02-05 23:56:30 +0900

    Add thread/start pkg

---
+++ src/thread/collection/start/HelloRunnable.java
@@ -0,0 +1,8 @@
+package thread.collection.start;
+
+public class HelloRunnable implements Runnable {
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + ": run()");
+	}
+}
